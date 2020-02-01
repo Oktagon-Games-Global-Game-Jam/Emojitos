240f08cbee0c658b50da84a58c87f3170365eedc
Emanuel Tavares <emanuelmaues@gmail.com>
2020-02-01 10:19:02 -0300

    Add Hand

---
+++ Assets/Prefabs/Scripts/Hand.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    [SerializeField] private JoystickIndex _playerIndex = 0;
+    [SerializeField] private DeltaTimeType _deltaTimeType;
+    [SerializeField, Range(1, 10)] private int _horizontalSensibility = 1;
+    [SerializeField, Range(1, 10)] private int _verticalSensibility = 1;
+
+    protected virtual void Update()
+    {
+        string[] joysticks = Input.GetJoystickNames();
+        string horizontalAxisName = string.Format("Horizontal {0}", (int)_playerIndex);
+        string verticalAxisName = string.Format("Vertical {0}", (int)_playerIndex);
+        float horizontalAxis = Input.GetAxisRaw(horizontalAxisName) * _horizontalSensibility;
+        float verticalAxis = Input.GetAxisRaw(verticalAxisName) * _verticalSensibility;
+        Vector2 handMovement = new Vector2(horizontalAxis, verticalAxis);
+        transform.Translate(handMovement * Utils.GetDeltaTime(_deltaTimeType), Space.World);
+
+        Debug.LogFormat("Horizontal: [{0}], Vertical: [{1}], Input Movement: {2}", horizontalAxisName, verticalAxisName, handMovement);
+    }
+}
---
+++ Assets/Prefabs/Scripts/Utils.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public enum DeltaTimeType
+{
+    None,
+    DeltaTime,
+    SmoothDeltaTime,
+    UnscaledDeltaTime,
+    FixedDeltaTime,
+    FixedUnscaledDeltaTime
+}
+
+public enum JoystickIndex
+{
+    Player1 = 0,
+    Player2 = 1,
+    Player3 = 2,
+    Player4 = 3
+}
+
+public class Utils
+{
+    public static float GetDeltaTime(DeltaTimeType deltaTimeType)
+    {
+        switch (deltaTimeType)
+        {
+            case DeltaTimeType.DeltaTime:
+                return Time.deltaTime;
+            case DeltaTimeType.SmoothDeltaTime:
+                return Time.smoothDeltaTime;
+            case DeltaTimeType.UnscaledDeltaTime:
+                return Time.unscaledDeltaTime;
+            case DeltaTimeType.FixedDeltaTime:
+                return Time.fixedDeltaTime;
+            case DeltaTimeType.FixedUnscaledDeltaTime:
+                return Time.fixedUnscaledDeltaTime;
+        }
+        return 1f;
+    }
+}

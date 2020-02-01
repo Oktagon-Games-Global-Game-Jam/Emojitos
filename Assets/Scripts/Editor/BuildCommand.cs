7cb883f02a9e0b1d3b18f96e54bb9da6493e109a
Youri T. K. K. Mattar <youri@youhide.com.br>
2020-02-01 18:24:55 -0300

    Create BuildCommand.cs

---
+++ Assets/Scripts/Editor/BuildCommand.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public static class BuildCommand {
+    static void PerformBuild() {
+        var options = new BuildPlayerOptions {
+            locationPathName = "Build/os_x/test-game.app",
+            scenes = new[] { "Assets/Scenes/SampleScene.unity" },
+            target = BuildTarget.StandaloneOSX
+        };
+
+        var report = BuildPipeline.BuildPlayer(options);
+        var summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded) {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            Debug.Log("Path: " + summary.outputPath);
+        }
+
+        if (summary.result == BuildResult.Failed) {
+            Debug.Log("Build failed");
+            EditorApplication.Exit(1);
+        }
+    }
+}

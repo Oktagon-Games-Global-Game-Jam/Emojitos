72359e557a4129e87793172feab08934e18d9472
Youri T. K. K. Mattar <youri@youhide.com.br>
2020-02-02 00:31:41 -0300

    Update BuildCommand.cs

--- Assets/Scripts/Editor/BuildCommand.cs
+++ Assets/Scripts/Editor/BuildCommand.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 using UnityEditor.Build.Reporting;
 
 public static class BuildCommand {
-    static void PerformBuild() {
+    static void PerformBuild_OSx() {
         var options = new BuildPlayerOptions {
             locationPathName = "Build/os_x/game.app",
             scenes = new[] { "Assets/Scenes/OnePlayer.unity" },
@@ -25,4 +25,44 @@ public static class BuildCommand {
             EditorApplication.Exit(1);
         }
     }
+    static void PerformBuild_Windows() {
+        var options = new BuildPlayerOptions {
+            locationPathName = "Build/windows",
+            scenes = new[] { "Assets/Scenes/OnePlayer.unity" },
+            target = BuildTarget.StandaloneWindows
+        };
+
+        var report = BuildPipeline.BuildPlayer(options);
+        var summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded) {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            Debug.Log("Path: " + summary.outputPath);
+        }
+
+        if (summary.result == BuildResult.Failed) {
+            Debug.Log("Build failed");
+            EditorApplication.Exit(1);
+        }
+    }
+    static void PerformBuild_Linux() {
+        var options = new BuildPlayerOptions {
+            locationPathName = "Build/linux",
+            scenes = new[] { "Assets/Scenes/OnePlayer.unity" },
+            target = BuildTarget.StandaloneLinux64
+        };
+
+        var report = BuildPipeline.BuildPlayer(options);
+        var summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded) {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            Debug.Log("Path: " + summary.outputPath);
+        }
+
+        if (summary.result == BuildResult.Failed) {
+            Debug.Log("Build failed");
+            EditorApplication.Exit(1);
+        }
+    }        
 }
